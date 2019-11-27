3037713d0f6f9d74f88c4ec9264119b7ebb4427d
chaymankala <chaithanya.mankala@gmail.com>
2019-11-27 09:15:14 +0530

    [update] sentitment added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require("cors");
 
+var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
+
 require('dotenv').config();
 
 const middlewares = require('./middlewares');
@@ -17,11 +20,21 @@ app.use(cors())
 app.use(morgan('dev'));
 app.use(helmet());
 
-const BASE_URL = "http://ec2-18-191-81-4.us-east-2.compute.amazonaws.com:8983/solr/IRF19P1/"
+const BASE_URL = "http://ec2-18-219-106-64.us-east-2.compute.amazonaws.com:8983/solr/IRF19P4/"
 
 app.get('/select', async (req, res) => {
   try {
-    return res.json(await rp.get(BASE_URL + "select" + objectToQuerystring(req.query), { json: true }));
+    let _result = await rp.get(BASE_URL + "select" + objectToQuerystring(req.query), { json: true });
+    if (_result && _result.response && _result.response.docs && _result.response.docs.length) {
+      for (let i of _result.response.docs) {
+        if (i.tweet_text && i.tweet_text[0]) {
+          i.sentiment = sentiment.analyze(i.tweet_text[0], {
+            language: i.metadata && i.metadata.iso_language_code && i.metadata.iso_language_code[0] ? i.metadata.iso_language_code[0] : "en"
+          });
+        }
+      }
+    }
+    return res.json(_result);
 
   }
   catch (e) {
